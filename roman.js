322634842aba62877eaae4bec151b72b057b4de0
Ryan Grissinger <ryan.grissinger@gmail.com>
2016-07-07 23:24:47 -0400

    added and solved test for case 5

--- roman.js
+++ roman.js
@@ -2,4 +2,7 @@ function roman(i){
     if(i == 1){
         return "I";
     }
+    if(i == 5){
+        return "V";
+    }
 }
--- roman_numeral_spec.js
+++ roman_numeral_spec.js
@@ -1,5 +1,8 @@
 describe('roman',function(){
     it('should return I for 1',function(){
         expect(roman(1)).toBe("I");
+        expect(roman(5)).toBe("V");
     });
+
+
 });
