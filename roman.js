990e5a6668e4957fa4935fbcd375be9c731e96fc
Ryan Grissinger <ryan.grissinger@gmail.com>
2016-07-08 11:14:43 -0400

    refactored recursive logic to satisfy all basic test cases

--- roman.js
+++ roman.js
@@ -1,25 +1,52 @@
-function roman(i){
-    var ret = '';
-    if(i == 1){
-        ret = "I";
+function roman(i,ret){
+    if(!ret){
+        ret = "";
     }
-    if(i == 5){
-        ret = "V";
+    //if(i == 1){
+    //    ret = "I";
+    //}
+    //if(i == 5){
+    //    ret = "V";
+    //}
+    //if(i == 10){
+    //    ret = "X";
+    //}
+    //if(i == 50){
+    //    ret = "L";
+    //}
+    //if(i == 100){
+    //    ret = "C";
+    //}
+    //if(i == 500){
+    //    ret = "D";
+    //}
+    //if(i == 1000){
+    //    ret = "M";
+    //}
+
+    if(i>=1000){
+        i=i-1000;
+        ret = ret + 'M' + roman(i,ret);
+    }else if(i>=500){
+        i=i-500;
+        ret = ret + 'D' + roman(i,ret);
+    }else if(i>=100){
+        i=i-100;
+        ret = ret + 'C' + roman(i,ret);
+    }else if(i>=50){
+        i=i-50;
+        ret = ret + 'L' + roman(i,ret);
+    }else if(i>=10){
+        i=i-10;
+        ret = ret + 'X' + roman(i,ret);
+    }else if(i>=5){
+        i=i-5;
+        ret = ret + 'V' + roman(i,ret);
+    }else if(i>=1){
+        i=i-1;
+        ret = ret + 'I' + roman(i,ret);
     }
-    if(i == 10){
-        ret = "X";
-    }
-    if(i == 50){
-        ret = "L";
-    }
-    if(i == 100){
-        ret = "C";
-    }
-    if(i == 500){
-        ret = "D";
-    }
-    if(i == 1000){
-        ret = "M";
-    }
+
     return ret;
 }
+
