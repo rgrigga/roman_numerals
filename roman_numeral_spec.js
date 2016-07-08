478bce2c36d1caa79dabdac073d234d3f431a05f
Ryan Grissinger <ryan.grissinger@gmail.com>
2016-07-08 19:33:15 -0400

    added all my other fringe cases

--- roman_numeral_spec.js
+++ roman_numeral_spec.js
@@ -49,25 +49,25 @@ describe('roman to arabic',function(){
 
     var cases = {
         1:'I',
-        //2:'II',
+        2:'II',
         3:'III',
-        //4:'IV',
-        //5:'V',
-        //8:'VIII',
+        4:'IV',
+        5:'V',
+        8:'VIII',
         9:'IX',
-        //10:'X',
-        //14:'XIV',
-        //50:'L',
-        //99:'XCIX',
-        //100:'C',
-        //500:'D',
+        10:'X',
+        14:'XIV',
+        50:'L',
+        99:'XCIX',
+        100:'C',
+        500:'D',
         900:'CM',
-        //999:'CMXCIX',
-        //1000:'M',
+        999:'CMXCIX',
+        1000:'M',
         1066:'MLXVI',
-        //1944:'MCMXLIV',
+        1944:'MCMXLIV',
         1989:'MCMLXXXIX',
-        //1999:'MCMXCIX'
+        1999:'MCMXCIX'
     };
 
     Object.keys(cases).forEach(function(key){
